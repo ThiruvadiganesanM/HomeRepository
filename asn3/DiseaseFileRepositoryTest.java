e954c0dcf0a8a56d36b74ae4a6694638a9b20e53
ThiruvadiganesanM <138756571+ThiruvadiganesanM@users.noreply.github.com>
2023-08-22 23:56:48 +0530

    Add files via upload

---
+++ asn3/DiseaseFileRepositoryTest.java
@@ -0,0 +1,33 @@
+package edu.disease.asn3;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class DiseaseFileRepositoryTest {
+	Disease[] diseases = new Disease[2];
+	Patient[] patients = new Patient[2];
+
+	@Test
+	public void testSave() throws IOException {
+		DiseaseFileRepository dfr = new DiseaseFileRepository();
+		dfr.save(diseases, patients);
+		assertNotNull(dfr);
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void testInit() throws ClassNotFoundException, IOException {
+		String folderPath="D:\\Ganesh";
+		DiseaseFileRepository dfr = new DiseaseFileRepository();
+		assertNotNull(dfr.init(folderPath));
+	}
+}
